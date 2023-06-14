547da4f774438464b6b5f318ca87cc5359cdab17
Jesús <jesusherediacarrizal2021@gmail.com>
2023-06-14 19:17:36 -0300

    hola

---
+++ javaa.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".button-menu-toggle")
+  .addEventListener("click", function () {
+    document.querySelector(".nav-links").
+      classList.toggle("nav-links-responsive")
+  })
